8c673eebe700588d98af1e13cc4087ff8fe9c594
TimbirICT <tmiddlebrooks99@gmail.com>
2023-11-25 17:28:08 -0600

    added classes and constructors

--- index.js
+++ index.js
@@ -1,80 +1,111 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-async function promptUser() {
-    const responses = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'text',
-            message: 'Enter Logo Text (3 char. max)',
-        },
-        {
-        type: 'list',
-            name: 'shape',
-            message: 'Pick a Shape',
-            choices: [
-                { title: 'Circle', value: 'Circle' },
-                { title: 'Triangle', value: 'Triangle' },
-                { title: 'Square', value: 'Square' },
-            ]
-        },
-        {
-            type: 'input',
-            name: 'color',
-            message: 'What color would you like your logo to be?',
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: 'What color would you like your logo text to be?',
-        },
-   
-    ]);
-    return responses;
+class UserPrompter {
+    async promptUser() {
+        const responses = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'text',
+                message: 'Enter Logo Text (3 char. max)',
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'Pick a Shape',
+                choices: [
+                    { title: 'Circle', value: 'Circle' },
+                    { title: 'Triangle', value: 'Triangle' },
+                    { title: 'Square', value: 'Square' },
+                ]
+            },
+            {
+                type: 'input',
+                name: 'color',
+                message: 'What color would you like your logo to be?',
+            },
+            {
+                type: 'input',
+                name: 'textColor',
+                message: 'What color would you like your logo text to be?',
+            },
+        ]);
+        return responses;
+    }
 }
 
-function drawShape(shape, color) {
-    let shapeSVG = '';
-    if (shape === 'Circle') {
-        shapeSVG = `<circle cx="50" cy="50" r="40" fill="${color}" />`;
+class Triangle {
+    constructor(color) {
+        this.color = color;
     }
-    else if (shape === 'Triangle') {
-        shapeSVG = `<polygon points="20,20 60,80 100,20" fill="${color}" />`;
+
+    init() {
+        return `<polygon points="10,90 50,10 90,90" fill="${this.color}" />`;
     }
-    else if (shape === 'Square') {
-    }
-    return shapeSVG;
 }
 
-function generateLogo(data) {
-    if (data.text.length > 3) {
-        throw new Error('Logo text exceeds 3 characters');
+class Circle {
+    constructor(color) {
+        this.color = color;
     }
-    const shapeSVG = drawShape(data.shape, data.color);
-    const textSVG = `<text x="50" y="55" font-family="Arial" font-size="20" fill="${data.textColor}" text-anchor="middle">${data.text}</text>`;
-    return `<svg width="200" height="200" viewBox="0 0 100 100">${shapeSVG}${textSVG}</svg>`;
+
+    init() {
+        return `<circle cx="50" cy="50" r="40" fill="${this.color}" />`;
+    }
+}
+
+class Square {
+    constructor(color) {
+        this.color = color;
+    }
+
+    init() {
+        return `<rect x="0" y="0" width="100" height="100" fill="${this.color}" />`;
+    }
+}
+
+class LogoGenerator {
+    constructor(data) {
+        this.data = data;
+    }
+
+    generate() {
+        if (this.data.text.length > 3) {
+            throw new Error('Logo text exceeds 3 characters');
+        }
+
+        let shapeSVG;
+        switch (this.data.shape) {
+            case 'Circle':
+                shapeSVG = new Circle(this.data.color).init();
+                break;
+            case 'Square':
+                shapeSVG = new Square(this.data.color).init();
+                break;
+            case 'Triangle':
+                shapeSVG = new Triangle(this.data.color).init();
+                break;
+            default:
+                throw new Error('Invalid shape');
+        }
+
+        const textSVG = `<text x="50" y="55" font-family="Arial" font-size="20" fill="${this.data.textColor}" text-anchor="middle">${this.data.text}</text>`;
+        return `<svg width="200" height="200" viewBox="0 0 100 100">${shapeSVG}${textSVG}</svg>`;
+    }
 }
 
 async function init() {
     try {
-        const data = await promptUser();
-        const logo = generateLogo(data);
+        const prompter = new UserPrompter();
+        const data = await prompter.promptUser();
+        const logoGenerator = new LogoGenerator(data);
+        const logo = logoGenerator.generate();
 
-        const outputDir = './output';
-        if (!fs.existsSync(outputDir)){
-            fs.mkdirSync(outputDir, { recursive: true });
-        }
-
-        fs.writeFile(`${outputDir}/logo.svg`, logo, (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('Logo generated successfully.');
-            }
-        });
+        fs.writeFileSync('logo.svg', logo);
+        console.log('Logo generated successfully.');
     } catch (error) {
-        console.error(error.message);
+        console.error(error);
     }
 }
 
- init();
+init();
